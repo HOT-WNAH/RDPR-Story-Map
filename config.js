128654af92bcf0293b45d3a52bd28c2d1c84a0da
michaelosunga <mikeotieno87@gmail.com>
2025-03-13 16:41:31 +0300

    storymap files
    mapbox storymap files

---
+++ config.js
@@ -0,0 +1,305 @@
+
+
+let topTitleDiv = "<h4>Open Mapping Hub | West and Northern Africa (WNAH) | In Collaboration with the Humanitarian OpenStreetMap Team</h4>";
+
+let titleDiv = "<h1>Harnessing Open Participatory Mapping for Proactive Disaster Preparedness</h1>";
+
+let bylineDiv = "<p>Open Mapping Hub - West and Northern</p>";
+
+let descriptionDiv =
+"<p>The Open Mapping Hub - West and Northern Africa (WNA Hub) engages open mapping organizations and communities in the region to facilitate the exchange of ideas and expertise across the region, and provide financial and technical support to massively scale local edits on OpenStreetMap. We stimulate and strengthen the leadership of communities to protect their livelihoods, improve their resilience, and preserve their way of life by promoting social justice in accessing open data and free software for their own purposes.</p>" +
+ "<p>The hub works with organizations and communities across 24 countries in creating and updating free, editable, and shareable open map data in OpenStreetMap. We envision a future where all people in West and Northern Africa have the opportunity to contribute to and use open mapping processes and open data in solving local challenges such as disasters.</p>" +
+
+'<p style="text-align:center">Scroll to continue<br>▼</p>';
+
+let footerDiv =
+'<p>This storymap is based on open map data from <a href="https://www.openstreetmap.org/#map=3/0.36/22.68" target="_blank">OpenStreetMap (OSM)</a></p>' +
+
+'<p><a href="https://www.mapbox.com/about/maps/" target="_blank">© Mapbox</a> | <a href="http://www.openstreetmap.org/about/" target="_blank">© OpenStreetMap</a></p>';
+
+let divChapter1 =
+  "<h3>Introduction: The Regional Disaster & Preparedness Project (RDPR)</h3>" +
+  '<img src="images/Chapter_1_Image.jpeg">' +
+  '<p class="imageCredit"><a href="https://www.hotosm.org/hubs/open-mapping-hub-west-and-northern-africa.html" target="_blank">Open Mapping Hub - West and Northern</a></p>' +
+"<p>The WNAH is enhancing disaster preparedness by leveraging the power of OpenStreetMap (OSM) to build resilient communities. Through participatory mapping initiatives, the hub collaborates with local volunteers, organizations, and governments to generate accurate, up-to-date geospatial data critical for disaster risk reduction and emergency response. By mapping vulnerable areas, infrastructure, and key resources, the initiative ensures that first responders and decision-makers have the information they need to act swiftly and effectively. This approach not only strengthens disaster resilience but also empowers communities to take an active role in safeguarding their environments through open, accessible mapping technologies.</p" +
+"<p>One of the main challenges in using OpenStreetMap (OSM) for disaster preparedness is determining where to initiate mapping efforts. Identifying high-risk areas requires a combination of historical disaster data, vulnerability assessments, and community insights. Limited availability of existing geospatial data in remote or underserved regions further complicates prioritization. Additionally, coordinating volunteers and ensuring the sustainability of mapping efforts can be difficult without strong local engagement and institutional support.</p>" +
+"<p>The disasters of natural origin preparedness and response in project (RDPR) focused on investigating disasters that occurred in the WNAH during the last decade, notably from 2014 to 2023. The report is the result of data collection and discussions with stakeholders identified at national levels. The data collection levels were based on desk review and phone exchange.</p>" +
+"<p>The findings from this project are intended to provide strategic guidance to WNAH on prioritizing areas for open participatory mapping in disaster preparedness. By identifying high-risk locations, data gaps, and community needs, the report aims to ensure that mapping efforts are targeted where they can have the greatest impact. These insights will help WNAH allocate resources effectively, engage local stakeholders, and enhance the availability of critical geospatial data to support disaster response and resilience-building initiatives.</p>"
+let divChapter2 =
+  "<h3>Disasters and impacts in the region</h3>" +
+'<img src="images/output_animation5.gif">' +
+'<p class="imageCredit"><a href="https://www.hotosm.org/hubs/open-mapping-hub-west-and-northern-africa.html" target="_blank">Open Mapping Hub - West and Northern Africa</a></p>' +
+"<p>Between 2014 and 2023, the WNAH experienced extreme natural events, primarily heavy rainfalls, flooding, storms, landslides, and occasional volcanic eruptions. These disasters led to widespread destruction, affecting millions of people through displacement, casualties, and economic losses. Countries such as Nigeria, Côte d'Ivoire, Cameroon, and Ghana were frequently impacted, with floods being the most recurrent hazard. In addition to the physical damage to homes, schools, and infrastructure, some events triggered secondary crises like cholera outbreaks, especially in Cameroon and Sierra Leone.</p>" +
+"<p>The deadliest years were 2019 and 2022, with millions affected, thousands of casualties, and vast agricultural losses. The period saw significant increases in displacement and destruction, particularly in 2022. The disasters also affected vital infrastructure, including water and sanitation systems, roads, and health facilities, further exacerbating humanitarian challenges. By 2023, the trend continued, with Morocco experiencing a rare earthquake and widespread flooding affecting multiple countries. The cumulative impact highlights the region's vulnerability to climate-related disasters and the urgent need for resilience-building measures.</p>"
+
+
+
+let divChapter3 =
+"<h3>Chapter 3: OpenStreetMap use cases in disaster response</h3>" +
+'<img src="images/Chapter_3_Image.jpeg">' +
+
+
+'<p class="imageCredit"><a href="https://www.hotosm.org/projects/morocco-earthquake-openstreetmap-data-in-support-of-response-efforts/" target="_blank">Morocco Earthquake: OpenStreetMap data in support of response efforts</a></p>' +
+"<p>OpenStreetMap (OSM) data has played a crucial role in disaster response efforts in both Morocco and Liberia. Following the devastating Morocco earthquake, humanitarian organizations and mapping volunteers quickly mobilized to update and enhance OSM data, providing responders with detailed maps of affected areas. This included mapping roads, buildings, and critical infrastructure to support search and rescue operations, logistics planning, and aid distribution. The effort ensured that emergency responders had up-to-date geographic information to navigate damaged regions efficiently and coordinate relief efforts effectively.</p>" +
+"<p>In Liberia, OSM data has been instrumental in tracking and mitigating flooding in rural areas, particularly in Robertsport. Local mapping initiatives have worked to collect geospatial data on flood-prone areas, infrastructure, and community resources. By integrating OSM data with real-time flood tracking, authorities and humanitarian groups can better predict flood risks, improve early warning systems, and design more effective response strategies. This proactive use of OSM enhances disaster preparedness, helping communities become more resilient against future flooding events.</p>"
+
+let divChapter4 =
+"<h3>Chapter 4:  Data Gaps in OSM during disaster response</h3>" +
+'<img src="images/Chapter_4_Image.jpg">' +
+'<p class="imageCredit"><a href="https://www.gdacs.org/Floods/report.aspx?eventid=1101617&episodeid=1&eventtype=FL" target="_blank">Overall green alert flood for Liberia from GDACS</a></p>' +
+"<p>OpenStreetMap (OSM) data plays a vital role in disaster response by providing real-time, crowdsourced geographic information for mapping affected areas, planning relief efforts, and coordinating emergency operations. However, data gaps in OSM often hinder effective disaster response.</p>" +
+"<p>From June 17 to mid-July 2022, heavy rains and flooding impacted several communities in Liberia’s Margibi and Montserrado counties, affecting over 89,315 people—primarily in Montserrado. The disaster destroyed numerous homes and three reported fatalities (with only one confirmed). The response efforts in these counties were significantly hampered by a lack of up-to-date information on buildings, roads, and essential infrastructure.</p>" +
+"<p>A similar disaster occurred in Southern and Central Nigeria in October 2022, resulting in 603 fatalities, over 2,400 injuries, and nearly 1.3 million displaced people, with more than 2.5 million affected nationwide. Bayelsa State was the hardest hit, with around 700,000 people displaced or affected. Reports also indicate that approximately 203,400 houses were damaged, including over 82,000 destroyed.</p>" +
+"<p>To overcome these challenges during disaster response, the WNAH mobilizes mapping communities through open participatory mapping, using remote and field mapping initiatives to rapidly generate crucial OSM data. This ensures that accurate geospatial information is available for humanitarian responders to assess disaster impacts and improve response efforts in affected regions.</p>"
+
+let divChapter5 =
+"<h3>Chapter 5: OSM mapping campaigns for disaster preparedness in the region</h3>" +
+'<img src="images/Chapter_5_Image.jpg">' +
+'<p class="imageCredit"><a href="https://www.hotosm.org/updates/mapping-for-el-nino-2023-early-warning-and-anticipatory-action/" target="_blank">Mapping for El Niño 2023 Early Warning and Anticipatory Action</a></p>' +
+"<p>WNAH is set to launch mapping campaigns in disaster-prone regions, aiming to generate crucial datasets in OpenStreetMap (OSM) with the involvement of local communities and relevant stakeholders. This initiative, aligned with the recommendations of the RDPR project, seeks to enhance disaster preparedness by crowdsourcing geographic data. Through the campaign dubbed #MappingforAnticipatoryAction, mappers from across the region and the world are encouraged to contribute, ensuring that critical infrastructure such as buildings, roads and amenities are mapped on OSM before disasters occur.</p>" +
+"<p>For #MappingforAnticipatoryAction, priority shall be given to Nigeria, Cameroon, Côte d'Ivoire, Niger, Central African Republic, Mali, Ghana, Sierra Leone, Liberia, and Morocco due to their frequent and severe disasters, including floods, storms, landslides, and disease outbreaks. Secondary priorities include Burkina Faso, Senegal, Togo, Benin, Guinea, Mauritania, Chad, Cape Verde, The Gambia, and Gabon, which also experience significant impacts but with slightly less frequency. Lower-priority countries such as Congo, São Tomé and Príncipe, and Guinea-Bissau face occasional disasters. Mapping efforts in the most affected nations will maximize disaster preparedness and response effectiveness.</p>" +
+"<p>Participants in the campaign will follow a structured mapping process to ensure efficiency and accuracy. First, they shall have to register through the provided form to stay connected with the campaign. Next, they will access the Tasking Manager to select a specific mapping campaigns in areas anticpated to experience disasters. Once a task is chosen, they will begin mapping, focusing on identifying the vulnerability of assets such as buildings, roads, and infrastructure. This data will be instrumental for humanitarian responders, enabling them to make informed decisions, optimize resource allocation, and ultimately save lives during disaster response efforts.</p>" 
+
+
+var config = {
+  style: "mapbox://styles/mapbox/streets-v11",
+  accessToken: "pk.eyJ1Ijoid25haHViIiwiYSI6ImNtN3QyOXk5NjB6cHgya3Nlbms4aDNoM3QifQ.wS7IUzHqdiIxkyNdu45kwA",
+  showMarkers: false,
+  markerColor: "#3FB1CE",
+  theme: "light",
+  use3dTerrain: false,
+  topTitle: topTitleDiv,
+  title: titleDiv,
+  subtitle: "",
+  byline: bylineDiv,
+  description: descriptionDiv,
+  footer: footerDiv,
+  chapters: [
+    {
+      id: "chapter1",
+      alignment: "right",
+      hidden: false,
+      chapterDiv: divChapter1,
+      location: {
+    
+
+        center: { lon: 6.2381, lat: 14.493 },
+        zoom: 2,
+        pitch: 0.00,
+        bearing: 0.00
+
+      },
+      mapAnimation: "flyTo",
+      rotateAnimation: false,
+      callback: "",
+      onChapterEnter: [
+        {layer: 'chapter3vector',opacity: 0},
+        {layer: 'chapter3vectorBorder',opacity: 0},
+        {layer: 'chapter3vectorLabels',opacity: 0},
+        {layer: 'chapter1vector',opacity: 1},
+        {layer: 'chapter1vectorBorder',opacity: 1},
+        {layer: 'chapter1vectorLabels',opacity: 1},
+        {layer: 'chapter5vector',opacity: 0},
+        {layer: 'chapter5vectorBorder',opacity: 0},
+        {layer: 'chapter5vectorLabels',opacity: 0},
+
+
+
+
+      ],
+      onChapterExit: [
+        {layer: 'chapter3vector',opacity: 0},
+        {layer: 'chapter3vectorBorder',opacity: 0},
+        {layer: 'chapter3vectorLabels',opacity: 0},
+        {layer: 'chapter1vector',opacity: 0},
+        {layer: 'chapter1vectorBorder',opacity: 0},
+        {layer: 'chapter1vectorLabels',opacity: 0},
+        {layer: 'chapter5vector',opacity: 0},
+        {layer: 'chapter5vectorBorder',opacity: 0},
+        {layer: 'chapter5vectorLabels',opacity: 0},
+
+
+
+
+      ],
+    },
+
+    {
+      id: "chapter2",
+      alignment: "centre",
+      hidden: false,
+      title: "",
+      image: "",
+      description: "",
+      chapterDiv: divChapter2,
+      location: {
+        center: { lon: 7.0180, lat: 11.46775 },
+        zoom: 3.42,
+        pitch: 0.00,
+        bearing: 0.00
+      },
+      mapAnimation: "flyTo",
+      rotateAnimation: false,
+      callback: () => {
+        document.getElementById("walkability-legend").style.visibility = "visible";
+    },
+      onChapterEnter: [
+        {layer: 'chapter3vector',opacity: 0},
+        {layer: 'chapter3vectorBorder',opacity: 0},
+        {layer: 'chapter3vectorLabels',opacity: 0},
+        {layer: 'chapter1vector',opacity: 0},
+        {layer: 'chapter1vectorBorder',opacity: 0},
+        {layer: 'chapter1vectorLabels',opacity: 0},
+        {layer: 'chapter5vector',opacity: 0},
+        {layer: 'chapter5vectorBorder',opacity: 0},
+        {layer: 'chapter5vectorLabels',opacity: 0},
+
+
+
+      ],
+      onChapterExit: [
+        {layer: 'chapter3vector',opacity: 0},
+        {layer: 'chapter3vectorBorder',opacity: 0},
+        {layer: 'chapter3vectorLabels',opacity: 0},
+        {layer: 'chapter1vector',opacity: 0},
+        {layer: 'chapter1vectorBorder',opacity: 0},
+        {layer: 'chapter1vectorLabels',opacity: 0},
+        {layer: 'chapter5vector',opacity: 0},
+        {layer: 'chapter5vectorBorder',opacity: 0},
+        {layer: 'chapter5vectorLabels',opacity: 0},
+
+
+
+      ],
+    },
+    {
+      id: "chapter3",
+      alignment: "right",
+      hidden: false,
+      title: "",
+      image: "",
+      description: "",
+      chapterDiv: divChapter3,
+      location: {
+        center: { lon: -13.08, lat: 16.76 },
+        zoom: 3.42,
+        pitch: 0.00,
+        bearing: 0.00
+      },
+      mapAnimation: "flyTo",
+      rotateAnimation: false,
+      callback: "",
+      onChapterEnter: [
+      
+        {layer: 'chapter3vector',opacity: 1},
+        {layer: 'chapter3vectorBorder',opacity: 1},
+        {layer: 'chapter3vectorLabels',opacity: 1},
+
+        {layer: 'chapter1vector',opacity: 0},
+        {layer: 'chapter1vectorBorder',opacity: 0},
+        {layer: 'chapter1vectorLabels',opacity: 0},
+        {layer: 'chapter5vector',opacity: 0},
+        {layer: 'chapter5vectorBorder',opacity: 0},
+        {layer: 'chapter5vectorLabels',opacity: 0},
+        
+      ],
+      onChapterExit: [
+        {layer: 'chapter3vector',opacity: 0},
+        {layer: 'chapter3vectorBorder',opacity: 0},
+        {layer: 'chapter3vectorLabels',opacity: 0},
+        {layer: 'chapter1vector',opacity: 0},
+        {layer: 'chapter1vectorBorder',opacity: 0},
+        {layer: 'chapter1vectorLabels',opacity: 0},
+        {layer: 'chapter5vector',opacity: 0},
+        {layer: 'chapter5vectorBorder',opacity: 0},
+        {layer: 'chapter5vectorLabels',opacity: 0},
+
+      ],
+    },
+    {
+      id: "chapter4",
+      alignment: "left",
+      hidden: false,
+      title: "",
+      image: "",
+      description: "",
+      chapterDiv: divChapter4,
+      location: {
+        center: { lon: 7.0180, lat: 11.46775 },
+        zoom: 3.42,
+        pitch: 0.00,
+        bearing: 0.00
+      },
+      mapAnimation: "flyTo",
+      rotateAnimation: false,
+      callback: "",
+      onChapterEnter: [
+        {layer: 'chapter3vector',opacity: 0},
+        {layer: 'chapter3vectorBorder',opacity: 0},
+        {layer: 'chapter3vectorLabels',opacity: 0},
+        {layer: 'chapter1vector',opacity: 0},
+        {layer: 'chapter1vectorBorder',opacity: 0},
+        {layer: 'chapter1vectorLabels',opacity: 0},
+        {layer: 'chapter5vector',opacity: 0},
+        {layer: 'chapter5vectorBorder',opacity: 0},
+        {layer: 'chapter5vectorLabels',opacity: 0},
+
+      ],
+      onChapterExit: [
+        {layer: 'chapter3vector',opacity: 0},
+        {layer: 'chapter3vectorBorder',opacity: 0},
+        {layer: 'chapter3vectorLabels',opacity: 0},
+        {layer: 'chapter1vector',opacity: 0},
+        {layer: 'chapter1vectorBorder',opacity: 0},
+        {layer: 'chapter1vectorLabels',opacity: 0},
+        {layer: 'chapter5vector',opacity: 0},
+        {layer: 'chapter5vectorBorder',opacity: 0},
+        {layer: 'chapter5vectorLabels',opacity: 0},
+
+      ],
+    },
+    {
+      id: "chapter5",
+      alignment: "right",
+      hidden: false,
+      title: "",
+      image: "",
+      description: "",
+      chapterDiv: divChapter5,
+      location: {
+        center: { lon: 15.0180, lat: 11.46775 },
+        zoom: 3.42,
+        pitch: 0.00,
+        bearing: 0.00
+      },
+      mapAnimation: "flyTo",
+      rotateAnimation: false,
+      callback: "",
+      onChapterEnter: [
+        {layer: 'chapter3vector',opacity: 0},
+        {layer: 'chapter3vectorBorder',opacity: 0},
+        {layer: 'chapter3vectorLabels',opacity: 0},
+        {layer: 'chapter1vector',opacity: 0},
+        {layer: 'chapter1vectorBorder',opacity: 0},
+        {layer: 'chapter1vectorLabels',opacity: 0},
+        {layer: 'chapter5vector',opacity: 1},
+        {layer: 'chapter5vectorBorder',opacity: 1},
+        {layer: 'chapter5vectorLabels',opacity: 1},
+
+      ],
+      onChapterExit: [
+        {layer: 'chapter3vector',opacity: 0},
+        {layer: 'chapter3vectorBorder',opacity: 0},
+        {layer: 'chapter3vectorLabels',opacity: 0},
+
+        {layer: 'chapter1vector',opacity: 0},
+        {layer: 'chapter1vectorBorder',opacity: 0},
+        {layer: 'chapter1vectorLabels',opacity: 0},
+        {layer: 'chapter5vector',opacity: 0},
+        {layer: 'chapter5vectorBorder',opacity: 0},
+        {layer: 'chapter5vectorLabels',opacity: 0},
+      ],
+    },
+
+  ],
+};
